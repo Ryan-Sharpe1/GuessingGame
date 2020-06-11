31477474a18f9b838fbb56e0212ea49f6b98eb5f
Ryan <ryansharpe214@gmail.com>
2020-06-11 10:55:31 +0100

    Added Questions

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -10,7 +10,11 @@ namespace GuessingGame
 
         static void Main(string[] args)
         {
-            string secretWord = "Penguin";
+            string[] animals = { "Penguin", "Elephant", "Giraffe", "Lion", "Tiger", "Bear", "Crocodile", "Dolphin", "Shark", "Eagle", "Ostrich", "LLama" };
+
+            Random r = new Random();
+
+            string secretWord = animals[r.Next(0, animals.Length)];
 
             int guessLimit = 3;
             int guessCount = 0;
@@ -37,13 +41,13 @@ namespace GuessingGame
 
                 if (guessorQuestion == "guess" | guessorQuestion == "Guess")
                 {
-                    guessCount++;
                     GuessingGames(secretWord);
+                    guessCount++;
                 }
                 if (guessorQuestion == "Question" | guessorQuestion == "question")
                 {
-                    questionCount++;
                     QuestionTime(secretWord);
+                    questionCount++;
                 }
                 else
                 {
@@ -58,8 +62,11 @@ namespace GuessingGame
             }
 
             if (guessCount >= guessLimit)
+
             {
-                Console.WriteLine("You Lose!");
+                Console.WriteLine("You Lose!\n The Answer was " + secretWord);
+                Environment.Exit(0);
+
             }
 
         }
@@ -76,6 +83,7 @@ namespace GuessingGame
             if (answer == guess)
             {
                 Console.WriteLine("\nYou Win!\n");
+                Environment.Exit(0);
             }
             else
             {
@@ -89,12 +97,27 @@ namespace GuessingGame
             string answer = secretWord;
 
             string[] biggerThanFridge = { "Elephant", "Giraffe", "Lion", "Tiger", "Bear", "Crocodile" };
+            string[] animalHasLegs = {"Elephant", "Giraffe", "Lion", "Tiger", "Bear", "Crocodile", "Penguin", "LLama", "Ostrich", "Eagle" };
+            string[] animalHasWings = {"Ostrich", "Eagle" };
+            string[] animalCanSwim = {"Shark", "Dolphin" };
+            string[] animalHasLongNeck = {"Giraffe", "LLama" };
             bool isBigAnimal = false;
+            bool hasLegs = false;
+            bool hasWings = false;
+            bool canSwim = false;
+            bool hasLongNeck = false;
 
 
-            Console.WriteLine("\nQuestion Options : " + "\n 1: Is it bigger than a fridge?\n");
+
+            Console.WriteLine("\nQuestion Options : \n " +
+                "1: Is it bigger than a fridge?\n " +
+                "2: Does it have Legs?\n " +
+                "3: Does it have wings?\n " +
+                "4: Can it Swim?\n " +
+                "5: Does it have a long neck?\n ");
 
             int questionOption = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
 
             if (questionOption == 1)
@@ -111,10 +134,77 @@ namespace GuessingGame
                         isBigAnimal = false;
                     }
                 }
-                    Console.WriteLine(isBigAnimal);
+                Console.WriteLine("1: Is it bigger than a fridge? " + isBigAnimal);
+                Console.WriteLine("");
             }
-
-
+            if (questionOption == 2)
+            {
+                foreach (var animalLegs in animalHasLegs)
+                {
+                    if (answer == animalLegs)
+                    {
+                        hasLegs = true;
+                        break;
+                    }
+                    else
+                    {
+                        hasLegs = false;
+                    }
+                }
+                Console.WriteLine("2: Does it have Legs? " + hasLegs);
+                Console.WriteLine("");
+            }
+            if (questionOption == 3)
+            {
+                foreach (var animalWings in animalHasWings)
+                {
+                    if (answer == animalWings)
+                    {
+                        hasWings = true;
+                        break;
+                    }
+                    else
+                    {
+                        hasWings = false;
+                    }
+                }
+                Console.WriteLine("3: Does it have wings? " + hasWings);
+                Console.WriteLine("");
+            }
+            if (questionOption == 4)
+            {
+                foreach (var animalSwim in animalCanSwim)
+                {
+                    if (answer == animalSwim)
+                    {
+                        canSwim = true;
+                        break;
+                    }
+                    else
+                    {
+                        canSwim = false;
+                    }
+                }
+                Console.WriteLine("4: Can it Swim? " + canSwim);
+                Console.WriteLine("");
+            }
+            if (questionOption == 5)
+            {
+                foreach (var animalNeck in animalHasLongNeck)
+                {
+                    if (answer == animalNeck)
+                    {
+                        hasLongNeck = true;
+                        break;
+                    }
+                    else
+                    {
+                        hasLongNeck = false;
+                    }
+                }
+                Console.WriteLine("5: Does it have a long neck? " + hasLongNeck);
+                Console.WriteLine("");
+            }
 
         }
     }
